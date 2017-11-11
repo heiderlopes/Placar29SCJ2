70d9d345aa3692aead9fb7c7899ee916eb52ec27
logonpf <logonpf@fiap.com.br>
2017-11-11 12:28:44 -0200

    Login

--- app/src/main/java/br/com/fiap/placar/SplashActivity.java
+++ app/src/main/java/br/com/fiap/placar/SplashActivity.java
@@ -33,7 +33,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent proximaTela = new Intent(SplashActivity.this,
-                        MainActivity.class);
+                        LoginActivity.class);
                 startActivity(proximaTela);
                 SplashActivity.this.finish();
             }
